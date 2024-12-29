4a53b409a4ce3de2b47c09a521f64a241130d135
DESKTOP-D9JTQ1K\Administrator <nmihin@gmail.com>
2024-12-29 16:45:41 +0100

    fix

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,12 +5,11 @@ import Map from './Map';
 function Content() {
   return (
     <main className="App-content">
-      <h2>Dobrodošli u SynergyPlus</h2>
       <p>
-          SynergyPlus je napredni alat za logistiku transporta sirovina, dizajniran kako bi vam pomogao u pronalaženju optimalnih ruta i dobavljača. Ova aplikacija omogućuje pregled ključnih podataka o sirovinama putem interaktivne karte, pružajući korisnicima precizne i prilagođene informacije za učinkovito donošenje odluka.
+        Dobrodošli u <b>SynergyPlus</b>, napredni alat za logistiku transporta sirovina, dizajniran kako bi vam pomogao u pronalaženju optimalnih ruta i dobavljača. Ova aplikacija omogućuje pregled ključnih podataka o sirovinama putem interaktivne karte, pružajući korisnicima precizne i prilagođene informacije za učinkovito donošenje odluka.
       </p>
       <p>
-        SynergyPlus obuhvaća širok raspon sirovina, uključujući: <b><span class="material-clay">keramička i vatrostalna glina</span>, <span class="material-stone">tehničko-građevni kamen</span>, <span class="material-sand">građevni pijesak i šljunak</span>, <span class="material-brick"> ciglarska glina </span>, <span class="material-mineral"> karbonatne mineralne sirovine za industrijsku preradbu</span>.</b>
+      <b>SynergyPlus</b> obuhvaća širok raspon sirovina, uključujući: <b><span class="material-clay">keramička i vatrostalna glina</span>, <span class="material-stone">tehničko-građevni kamen</span>, <span class="material-sand">građevni pijesak i šljunak</span>, <span class="material-brick"> ciglarska glina </span>, <span class="material-mineral"> karbonatne mineralne sirovine za industrijsku preradbu</span>.</b>
       </p>
       <p>        Naš cilj je optimizirati vaše poslovanje kroz upotrebu najmodernijih tehnologija i inovativnih rješenja, olakšavajući upravljanje sirovinama i transportnim procesima.</p>
       <Map />
